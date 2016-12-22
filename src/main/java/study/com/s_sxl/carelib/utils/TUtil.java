53a17010d162b69929fefeaeafda2701f2d42cd5
LJDY490 <s_xiaole@163.com>
2016-12-22 15:10:23 +0800

    因为网络api的原因,采用模拟数据的方式显示视频

--- src/main/java/study/com/s_sxl/carelib/utils/TUtil.java
+++ src/main/java/study/com/s_sxl/carelib/utils/TUtil.java
@@ -8,9 +8,7 @@ import java.lang.reflect.ParameterizedType;
 public class TUtil {
     public static <T> T getT(Object o, int i) {
         try {
-            return ((Class<T>) ((ParameterizedType) (o.getClass()
-                    .getGenericSuperclass())).getActualTypeArguments()[i])
-                    .newInstance();
+            return ((Class<T>) ((ParameterizedType) (o.getClass().getGenericSuperclass())).getActualTypeArguments()[i]).newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
